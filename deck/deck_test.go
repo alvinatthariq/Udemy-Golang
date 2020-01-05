383212166a1e0ec4e10a9318b7b714bf228a2d09
alvinatthariq <54110905+alvinatthariq@users.noreply.github.com>
2020-01-05 22:21:07 +0700

    Add files via upload

---
+++ deck/deck_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"testing"
+	"os"
+)
+
+func TestNewDeck(t *testing.T) {
+	d := newDeck()
+
+	if len(d) != 9{
+		t.Errorf("Expected deck length of 9, but got %v", len(d))
+	}
+
+	if d[0] != "Ace of Spades"{
+		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
+	}
+
+	if d[len(d) - 1] != "Three of Hearts"{
+		t.Errorf("Expected last card of Three of Hearts, but got %v", d[len(d)-1])
+	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T){
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+	if len(loadedDeck) != 9{
+		t.Errorf("Expected 9 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
+}
